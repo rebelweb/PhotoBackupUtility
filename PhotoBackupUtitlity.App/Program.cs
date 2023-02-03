272d8525b6d745d84e157a0c10b9d22d6b5dac74
Ryan Condron <rebelwebdevelopment@gmail.com>
2023-02-02 22:30:16 -0600

    Setup Dependency Injection Container

--- PhotoBackupUtitlity.App/Program.cs
+++ PhotoBackupUtitlity.App/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace PhotoBackupUtility.App;
+﻿using Amazon.S3;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PhotoBackupUtility.App;
 
 public static class Program
 {
 	public static void Main(string[] args)
 	{
 		Console.WriteLine("Backing Up Your Photos");
+
+		ServiceCollection serviceCollection = new ServiceCollection();
+		RegisterServices(serviceCollection);
+
+		ServiceProvider provider = serviceCollection.BuildServiceProvider();
+		
+		Application app = provider.GetService<Application>();
+		
+		app.Call();
+	}
+
+	private static IServiceCollection RegisterServices(IServiceCollection services)
+	{
+		services.AddScoped<IAmazonS3>(provider => new AmazonS3Client());
+		services.AddScoped<IBackupStateService, BackupStateService>();
+		services.AddScoped<Application>();
+		return services;
 	}
 }
