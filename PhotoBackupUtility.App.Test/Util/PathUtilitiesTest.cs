57a2fea796b2ea1fa3ba8460ffb3a144e7b2a7bb
Ryan Condron <rebelwebdevelopment@gmail.com>
2023-02-11 20:42:20 -0600

    Add utility method to create S3 key based on file path

---
+++ PhotoBackupUtility.App.Test/Util/PathUtilitiesTest.cs
@@ -0,0 +1,15 @@
+﻿using PhotoBackupUtility.App.Util;
+
+namespace PhotoBackupUtility.App.Test.Util;
+
+public class PathUtilitiesTest
+{
+    [Theory]
+    [InlineData("E:\\Pictures\\2022\\2022-01-01\\101_0010.NEF", "Pictures\\2022\\2022-01-01\\101_0010.NEF")]
+    [InlineData("Pictures\\2022\\2022-01-01\\101_0010.NEF", "Pictures\\2022\\2022-01-01\\101_0010.NEF")]
+    [InlineData("/Pictures/2022/2022-01-01/100_0010.NEF", "Pictures/2022/2022-01-01/100_0010.NEF")]
+    public void TestS3KeyFromPath(string input, string output)
+    {
+        Assert.Equal(output, PathUtilities.S3ObjectKeyFromPath(input));
+    }
+}
---
+++ PhotoBackupUtitlity.App/Util/PathUtilities.cs
@@ -0,0 +1,15 @@
+﻿namespace PhotoBackupUtility.App.Util;
+
+public static class PathUtilities
+{
+    public static string S3ObjectKeyFromPath(string path)
+    {
+        if (!Path.IsPathRooted(path)) return path;
+
+        if (path.Substring(1,1) == ":") return path.Substring(3);
+
+        if (path.Substring(0, 1) == "/") return path.Substring(1);
+
+        return path;
+    }
+}
